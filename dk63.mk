4c0691035e35b3be3ef454e29b472049d77a9d23
Stephen Peery <smp4488@aol.com>
2020-01-05 22:15:36 -0500

    Merging master

--- dk63.mk
+++ dk63.mk
@@ -2,18 +2,17 @@
 
 # Put device into bootloader mode
 openocd:
-	openocd -s ../dk63/ -f stlink.cfg &
+	"openocd.exe" "-c" "gdb_port 3333" "-s" "C:\Users\smplman\projects\qmk_firmware-19" "-f" "C:\Users\smplman\projects\dk63\stlink.cfg" "-f" "C:\Users\smplman\projects\dk63\vs11k09a-1.cfg"
 
 openocd-stop:
 	pkill openocd
 
 dfu:
-	python3 ./util/dk63/dfu.py
+	python3 C:\Users\smplman\projects\qmk_firmware-19\util\dk63\dfu.py
 
 # run python script to upload the firmware
 upload:
-	python3 ./util/dk63/flash-firmware-hid.py ./.build/kmove_dk63_default.bin --vid 0x0c45 --pid 0x7040
-	# python3 ../dk63/files/RCData4000.bin ./.build/kmove_dk63_default.bin --vid 0x0c45 --pid 0x7040
+	python3 C:\Users\smplman\projects\qmk_firmware-19\util\dk63\flash-firmware.py C:\Users\smplman\projects\qmk_firmware-19\.build\kmove_dk63_default.bin --vid 0x0c45 --pid 0x7040
 
 gdb:
-	arm-none-eabi-gdb ./.build/kmove_dk63_default.elf -ex "target remote :3333" -ex "set confirm off" -ex "set pagination off"
+	arm-none-eabi-gdb.exe ./.build/kmove_dk63_default.elf -ex "target remote :3333" -ex "set confirm off" -ex "set pagination off"
