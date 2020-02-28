8b88eeacb30bd623871abcd93667faf2a05f5498
smp4488 <smp4488@aol.com>
2020-02-28 16:51:55 -0500

    some pwd updates, messing with the prescaler

--- keyboards/kemove/dk63/led_matrix.h
+++ keyboards/kemove/dk63/led_matrix.h
@@ -1,6 +1,6 @@
 
 
-void setup_led_pwm(void);
+// void setup_led_pwm(void);
 void set_pwm_values(uint8_t col, uint8_t row);
 void set_col_pwm(uint8_t col);
-void set_row_pwm(uint8_t row);
+void led_scan(void);
