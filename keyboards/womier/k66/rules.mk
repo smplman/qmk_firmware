0d600a65a9edcc3f4bae1d830a744fa788eb4339
toast <toast@killthought.com>
2020-04-19 17:29:26 -0400

    fix rgb toggle, light level add volume keys

--- keyboards/womier/k66/rules.mk
+++ keyboards/womier/k66/rules.mk
@@ -44,7 +44,7 @@ MAGIC_KEYCODE_ENABLE = no
 BOOTMAGIC_ENABLE = no  # Virtual DIP switch configuration
 ## (Note that for BOOTMAGIC on Teensy LC you have to use a custom .ld script.)
 MOUSEKEY_ENABLE = no # Mouse keys
-EXTRAKEY_ENABLE = no # Audio control and System control
+EXTRAKEY_ENABLE = yes # Audio control and System control
 CONSOLE_ENABLE = no  # Console for debug
 COMMAND_ENABLE = no    # Commands for debug and configuration
 SLEEP_LED_ENABLE = no  # Breathing sleep LED during USB suspend
